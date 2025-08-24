7f6a567a037322f38b859cf9756acd53a85abc3f
AlanNapieralski <alan.napieralski03@gmail.com>
2025-08-25 00:23:10 +0100

    added an exec file

--- install_dependencies.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-echo Installing game dependencies (Pygame and NumPy)...
-pip install pygame numpy
-echo.
-echo Installation complete! You can now run the game.
-pause
--- install_dependencies.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-echo "Installing game dependencies (Pygame and NumPy)..."
-pip3 install pygame numpy
-echo ""
-echo "Installation complete! You can now run the game."
